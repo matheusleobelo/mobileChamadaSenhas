0c3710a7703dbffe1ce8512bc23fde71aeb90431
matheusleobelo <bipoladogamer@gmail.com>
2023-10-24 14:20:23 -0300

    Faltando resolver a função na tab 2

--- src/app/services/senhas.service.ts
+++ src/app/services/senhas.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SenhasService {
   public senhasGeral: number = 0;
@@ -9,44 +9,58 @@ export class SenhasService {
   public senhasExame: number = 0;
   public senhasTotal: number = 0;
   public inputNovaSenha: string = '';
-  public senhasArray: { [key: string]: string[] } = { 'SG': [], 'SP': [], 'SE': [] } as { [key: string]: string[] };
+  public senhasArray: { [key: string]: string[] } = {
+    SG: [],
+    SP: [],
+    SE: [],
+  } as { [key: string]: string[] };
+  public senhasChamadas:string[] = [];
   public primeiroValor: string = '';
 
-  somaGeral() { this.senhasGeral++; this.senhasTotal++; }
-  somaPrior() { this.senhasPrior++; this.senhasTotal++; }
-  somaExame() { this.senhasExame++; this.senhasTotal++; }
+  somaGeral() {
+    this.senhasGeral++;
+    this.senhasTotal++;
+  }
+  somaPrior() {
+    this.senhasPrior++;
+    this.senhasTotal++;
+  }
+  somaExame() {
+    this.senhasExame++;
+    this.senhasTotal++;
+  }
 
-  constructor() { }
-
+  constructor() {}
 
   novaSenha(tipoSenha: string = ''): void {
     if (tipoSenha == 'SG') {
       this.somaGeral();
       this.inputNovaSenha =
-        new Date().getFullYear().toString().substring(2, 4) + '/' +
-        (new Date().getMonth() + 1).toString().padStart(2, '0') + '/'+
-        new Date().getDate().toString().padStart(2, '0') +
-        '-' +
+        new Date().getHours().toString().padStart(2, '0') +
+        ':' +
+        (new Date().getMinutes() + 1).toString().padStart(2, '0') +
+        ' - ' +
         tipoSenha +
         (this.senhasArray['SG'].length + 1).toString().padStart(2, '0');
+
       this.senhasArray['SG'].push(this.inputNovaSenha);
     } else if (tipoSenha == 'SP') {
       this.somaPrior();
       this.inputNovaSenha =
-        new Date().getFullYear().toString().substring(2, 4) + '/' +
-        (new Date().getMonth() + 1).toString().padStart(2, '0') + '/' +
-        new Date().getDate().toString().padStart(2, '0') +
-        '-' +
+        new Date().getHours().toString().padStart(2, '0') +
+        ':' +
+        (new Date().getMinutes() + 1).toString().padStart(2, '0') +
+        ' - ' +
         tipoSenha +
         (this.senhasArray['SP'].length + 1).toString().padStart(2, '0');
       this.senhasArray['SP'].push(this.inputNovaSenha);
     } else if (tipoSenha == 'SE') {
       this.somaExame();
       this.inputNovaSenha =
-        new Date().getFullYear().toString().substring(2, 4) + '/' +
-        (new Date().getMonth() + 1).toString().padStart(2, '0') + '/' +
-        new Date().getDate().toString().padStart(2, '0') +
-        '-' +
+        new Date().getHours().toString().padStart(2, '0') +
+        ':' +
+        (new Date().getMinutes() + 1).toString().padStart(2, '0') +
+        ' - ' +
         tipoSenha +
         (this.senhasArray['SE'].length + 1).toString().padStart(2, '0');
       this.senhasArray['SE'].push(this.inputNovaSenha);
@@ -57,17 +71,16 @@ export class SenhasService {
   chamarSenha() {
     if (this.senhasArray['SP'].length !== 0) {
       this.primeiroValor = this.senhasArray['SP'][0];
+      this.senhasChamadas.push(this.primeiroValor)
       this.senhasArray['SP'].shift();
-    } else if(this.senhasArray['SE'].length !== 0) {
+    } else if (this.senhasArray['SE'].length !== 0) {
       this.primeiroValor = this.senhasArray['SE'][0];
+      this.senhasChamadas.push(this.primeiroValor)
       this.senhasArray['SE'].shift();
-    }
-      else if(this.senhasArray['SG'].length !== 0) {
+    } else if (this.senhasArray['SG'].length !== 0) {
       this.primeiroValor = this.senhasArray['SG'][0];
+      this.senhasChamadas.push(this.primeiroValor)
       this.senhasArray['SG'].shift();
     }
   }
-  
-
-  
 }
